a1a445776adc9959b4ccb9f70d7912263fc6f781
anaelorlinski <ao@m9x.fr>
2022-02-12 00:25:07 +0100

    fix

--- openwrt-21.02/steps/04-prepare_package.sh
+++ openwrt-21.02/steps/04-prepare_package.sh
@@ -31,12 +31,14 @@ cp -R ../stangri_repo/vpn-policy-routing feeds/packages/net/
 #./scripts/feeds install -p stangri_repo luci-app-vpn-policy-routing
 
 # Time stamp with $Build_Date=$(date +%Y.%m.%d)
-echo "Build date github action $BUILD_DATE"
-echo -e '\nAO Build@'$(date "+%Y.%m.%d")'\n'  >> package/base-files/files/etc/banner
-sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_REVISION='$(date "+%Y.%m.%d")'" >> package/base-files/files/etc/openwrt_release
+MANUAL_DATE="$(date +%Y.%m.%d) (manual build)"
+BUILD_DATE=${BUILD_DATE:-$MANUAL_DATE}
+echo "Write build date in openwrt : $BUILD_DATE"
+echo -e '\nAO Build@'${BUILD_DATE}'\n'  >> package/base-files/files/etc/banner
+#sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+#echo "DISTRIB_REVISION='${BUILD_DATE}'" >> package/base-files/files/etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='AO Build@$(date "+%Y.%m.%d")'" >> package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='AO Build@${BUILD_DATE}'" >> package/base-files/files/etc/openwrt_release
 sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
 
 rm -rf .config
