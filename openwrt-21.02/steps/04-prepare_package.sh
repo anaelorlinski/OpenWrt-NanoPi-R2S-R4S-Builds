902d4104b20f270ccb7285e5cb294c3f579976d6
anaelorlinski <ao@m9x.fr>
2022-04-30 23:14:31 +0200

    add/replace acme/adblock with uacme/adguardhome

--- openwrt-21.02/steps/04-prepare_package.sh
+++ openwrt-21.02/steps/04-prepare_package.sh
@@ -25,10 +25,19 @@ cd openwrt
 # replace vpn routing packages
 rm -rf feeds/packages/net/vpn-policy-routing/
 cp -R ../stangri_repo/vpn-policy-routing feeds/packages/net/
+rm -rf feeds/luci/applications/luci-app-vpn-policy-routing
+cp -R ../stangri_repo/luci-app-vpn-policy-routing feeds/luci/applications/
+
+# add pbr
+cp -R ../stangri_repo/pbr feeds/packages/net/
+cp -R ../stangri_repo/luci-app-pbr feeds/luci/applications/
 
 # this does not work
 #./scripts/feeds uninstall luci-app-vpn-policy-routing
 #./scripts/feeds install -p stangri_repo luci-app-vpn-policy-routing
+
+# copy patch for nodejs not building
+cp $ROOTDIR/openwrt-21.02/patches/node/010-execvp-arg-list-too-long.patch feeds/packages/lang/node/patches/
 
 # Time stamp with $Build_Date=$(date +%Y.%m.%d)
 MANUAL_DATE="$(date +%Y.%m.%d) (manual build)"
