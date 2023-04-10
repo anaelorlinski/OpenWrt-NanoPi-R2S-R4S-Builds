013e89d9813ffaf8a11a011a25ecf21c23fbab08
anaelorlinski <ao@m9x.fr>
2023-04-10 18:47:59 +0200

    openwrt 22.03.4

--- openwrt-22.03/steps/02_prepare_openwrt_folder.sh
+++ openwrt-22.03/steps/02_prepare_openwrt_folder.sh
@@ -10,9 +10,9 @@ cd "$ROOTDIR/build"
 
 cp -R openwrt-fresh-22.03 openwrt
 
-# freeze revision to 22.03.3
+# freeze revision to 22.03.4
 cd openwrt
-git reset --hard 221fbfa2d854ccb6cd003c065ec308fbc0651b11
+git reset --hard 94adc23fa693d1b129ce1718573dfb12594f20f8
 
 echo "Current OpenWRT commit"
 git log -1
