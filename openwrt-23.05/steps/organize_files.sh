5a10d66e546e311566a73330804ca4d7c57865bb
stargazindreamr <49848622+stargazindreamr@users.noreply.github.com>
2024-06-11 22:59:34 -0400

    Update organize_files.sh

--- openwrt-23.05/steps/organize_files.sh
+++ openwrt-23.05/steps/organize_files.sh
@@ -15,10 +15,14 @@ RELTAG=$4
 
 mkdir -p ./artifact/
 mv build/openwrt/bin/targets/rockchip/armv8/*sysupgrade.img* ./artifact/
+mv build/openwrt/bin/targets/rockchip/armv8/*.bom.cdx.json ./artifact/
+mv build/openwrt/bin/targets/rockchip/armv8/*.manifest ./artifact/
 cd ./artifact/
 ls -Ahl
-mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-ext4-sysupgrade.img.gz OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-ext4.img.gz
-mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-squashfs-sysupgrade.img.gz OpenWrt-AO-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-squashfs.img.gz
+mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-ext4-sysupgrade.img.gz OpenWrt-NK-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-ext4.img.gz
+mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW-squashfs-sysupgrade.img.gz OpenWrt-NK-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG-squashfs.img.gz
+mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW.bom.cdx.json OpenWrt-NK-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG.bom.cdx.json
+mv openwrt-rockchip-armv8-friendlyarm_nanopi-$NANOPI_MODEL_LOW.manifest OpenWrt-NK-NanoPi$NANOPI_MODEL-$VARIANT-$OPENWRT_BRANCH-$RELTAG.manifest
 gzip -d *.gz && exit 0
 gzip --best *.img
 ls -Ahl
