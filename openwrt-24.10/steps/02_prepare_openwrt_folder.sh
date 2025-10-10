4061ff55a6df6e71408b2ced11eecdd1bb3d244f
Anael Orlinski <ao@m9x.fr>
2025-10-10 10:01:40 +0000

    openwrt 24.10.3

--- openwrt-24.10/steps/02_prepare_openwrt_folder.sh
+++ openwrt-24.10/steps/02_prepare_openwrt_folder.sh
@@ -10,9 +10,9 @@ cd "$ROOTDIR/build"
 
 cp -R openwrt-fresh-24.10 openwrt
 
-# freeze revision to 24.10.2
+# freeze revision to 24.10.3
 cd openwrt
-git reset --hard bb59922007043c0a0813d62b0d9f6e801caff9df
+git reset --hard dbd975a3b674c917d2f3b6663864084421944909
 
 echo "Current OpenWRT commit"
 git log -1
