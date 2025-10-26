a115a1cfee9b454985e3f2b05c4b4f7d921fb6e6
Anael Orlinski <ao@m9x.fr>
2025-10-26 13:42:02 +0000

    update telephony feed to include dahdi-linux patch

--- openwrt-24.10/steps/02_prepare_openwrt_folder.sh
+++ openwrt-24.10/steps/02_prepare_openwrt_folder.sh
@@ -13,6 +13,8 @@ cp -R openwrt-fresh-24.10 openwrt
 # freeze revision to 24.10.4
 cd openwrt
 git reset --hard 78b23a26c4c98938d549e7ff5876508544e33d4d
+# patch telephony feed to include last patch (remove after 24.10.4)
+sed -i 's/2a4541d46199ac96fac214d02c908402831c4dc6/11e9c73bff6be34ff2fdcd4bc0e81a4723d78652/g' feeds.conf.default
 
 echo "Current OpenWRT commit"
 git log -1
